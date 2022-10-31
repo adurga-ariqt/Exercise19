adf2d95e0d740068b9c7f36e87f88cab5f7785c7
adurga-ariqt <adurga@ariqt.com>
2022-10-31 12:06:29 +0530

    Add project files.

---
+++ Exercise19/triplesum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Exercise19
+{
+    internal class triplesum
+    {
+        static void Main(string[] args)
+        {
+            SumTriple();
+            
+           
+        }
+        public static void SumTriple()
+        {
+            int a, b ;
+            Console.WriteLine("Entervalues:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("entervalues:");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //b = Convert.ToInt32(Console.ReadLine());
+
+
+            //return a == b ? (a + b) * 3 : a + b;
+
+            if (a == b)
+            {
+               // (a + b) * 3;
+                Console.WriteLine((a + b)*3);
+            }
+            else
+            {
+               // (a + b);
+                Console.WriteLine(a + b);
+            }
+        }
+
+
+    }
+}
